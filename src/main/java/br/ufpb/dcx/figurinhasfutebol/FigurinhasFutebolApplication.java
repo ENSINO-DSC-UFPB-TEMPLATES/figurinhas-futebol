35a5d3b90ce7d1677ebc1e7cf4d37fef64c93c54
Daniel Faustino <daniel@dcx.ufpb.br>
2024-07-28 15:02:39 -0300

    Resolve conflict

---
+++ src/main/java/br/ufpb/dcx/figurinhasfutebol/FigurinhasFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.figurinhasfutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigurinhasFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FigurinhasFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufpb/dcx/figurinhasfutebol/FigurinhasFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.figurinhasfutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigurinhasFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
